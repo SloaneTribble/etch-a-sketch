71a7777e5ec081ba8204509d98eac3cc3a8bf538
Sloane Tribble <SloaneTribble@gmail.com>
2022-04-08 14:29:14 -0700

    Remove junk, add event listener

--- javascript.js
+++ javascript.js
@@ -6,24 +6,26 @@ gridContainer.classList.add('row');
 gridContainer.classList.add('column');
 
 for(let i = 0; i < dimension; i++){
+
     const row = document.createElement('div');
-    row.style.display = "flex";
-    row.style.flexDirection = "column";
-    row.style.justifyContent = "space-between";
+    row.style.borderColor = "black";
     row.style.borderWidth = "5px";
-    row.style.borderColor = "blue";
-    row.style.margin = "5px";
 
     for(j = 0; j < dimension; j++){
-        const column = document.createElement('div')
+        const column = document.createElement('div');
+        column.style.width = "100%";
         column.style.padding = "100%";
         column.style.margin = "5px";
         column.style.textAlign = "center";
-        column.innerText = "hey";
         column.style.height = "5px";
         column.style.width = "5px";
+        column.style.borderWidth = "5px";
+        column.style.borderColor = "black";
         column.style.backgroundColor = "pink";
         row.appendChild(column);
+        column.addEventListener("mouseover", () => {
+            column.style.backgroundColor = "white";
+        })
     }
     gridContainer.appendChild(row);
 }
@@ -31,5 +33,7 @@ for(let i = 0; i < dimension; i++){
 gridContainer.style.display = "flex";
 gridContainer.style.flex = "1";
 gridContainer.style.justifyContent = "space-between";
+
+
 
 
