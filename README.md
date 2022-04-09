f894319741de264788f926d60ea90600e53b0712
Sloane Tribble <SloaneTribble@gmail.com>
2022-04-09 12:45:39 -0700

    Prevent grid cell resizing based on cell count

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # etch-a-sketch
+This assignment is from The Odin Project, and is designed to practice DOM manipulation with JavaScript.  
--- javascript.js
+++ javascript.js
@@ -1,27 +1,27 @@
-let dimension = 100;
+let dimension = 10;
 
 
 const gridContainer = document.querySelector(".grid-container");
-gridContainer.classList.add('row');
-gridContainer.classList.add('column');
+gridContainer.style.display = "flex";
+gridContainer.style.flexDirection = "column";
+gridContainer.style.alignSelf = "center";
+gridContainer.style.justifySelf = "center";
+gridContainer.style.width = "500px";
+gridContainer.style.height = "500px";
 
 for(let i = 0; i < dimension; i++){
 
     const row = document.createElement('div');
     row.style.display = "flex";
-    row.style.flexDirection = "column";
-    row.style.borderColor = "black";
-    row.style.borderWidth = "5px";
+    row.style.flex = "1";
+    row.style.flexFlow = "row-wrap";
 
     for(j = 0; j < dimension; j++){
         const column = document.createElement('div');
-        column.style.width = "5px"; // 
-        column.style.padding = "5px"; // Removing lines 18-20 causes squares to disappear
-        column.style.margin = "5px"; //
-        column.style.flexGrow = "1";
-        column.style.flexShrink = "1";
-        column.style.borderWidth = "5px";
-        column.style.borderColor = "black";
+        // column.style.minWidth = "1px"; // 
+        // column.style.minHeight = "1px"; // Removing lines 18-19 causes squares to disappear
+        column.style.flex = "1";
+        column.style.border = "thin solid black";
         column.style.backgroundColor = "pink";
         row.appendChild(column);
         column.addEventListener("mouseover", () => {
@@ -31,12 +31,22 @@ for(let i = 0; i < dimension; i++){
     gridContainer.appendChild(row);
 }
 
-const outerContainer = document.querySelector("outer-container");
+const outerContainer = document.querySelector(".outer-container");
+
+outerContainer.style.width = "500px";
+outerContainer.style.height = "500px";
+outerContainer.style.display = "flex";
+outerContainer.style.flex = "1";
+
+document.body.appendChild(outerContainer);
 
 
-gridContainer.style.display = "flex";
-gridContainer.style.flexGrow = "1";
-gridContainer.style.justifyContent = "space-between";
+
+
+
+
+
+
 
 
 
